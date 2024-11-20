1b10ac68bb94ae1dfc38fc8a6e5731623bfc9dda
Tinah <Tinah@gmail.com>
2024-11-21 00:00:59 +0200

    colour

--- ProgPoePart2_6212/obj/Debug/net8.0/ProgPoePart2_6212.AssemblyInfo.cs
+++ ProgPoePart2_6212/obj/Debug/net8.0/ProgPoePart2_6212.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgPoePart2_6212")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1987ed7366938dc6ca9f616c8c231dd4a682a08b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cef9f4e7e767c238fbf6ad1c53abe93331f05af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgPoePart2_6212")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgPoePart2_6212")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
